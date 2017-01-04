bb74fec1c66e1aa5bb2c6995f5c0dfaab051acb0
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-04 15:48:35 -0500

    edit version

--- lib/two_cents/cli.rb
+++ lib/two_cents/cli.rb
@@ -1,22 +1,22 @@
 module TwoCents
   class Cli < Thor
-    PATH = "/Applications/Google Chrome.app"
+    OSX_PATH = "/Applications/Google Chrome.app"
     default_task :search
 
     desc "search QUERY", "searches Google for your query"
     def search(query = nil)
       if query
-        Kernel.exec "open", PATH, "https://www.google.com/#q=#{query}"
+        Kernel.exec "open", OSX_PATH, "https://www.google.com/#q=#{query}"
       else
-        Kernel.exec "open", PATH
+        Kernel.exec "open", OSX_PATH
       end
     end
 
     def self.search(query = nil)
       if query
-        Kernel.exec "open", PATH, "https://www.google.com/#q=#{query}"
+        Kernel.exec "open", OSX_PATH, "https://www.google.com/#q=#{query}"
       else
-        Kernel.exec "open", PATH
+        Kernel.exec "open", OSX_PATH
       end
     end
   end
