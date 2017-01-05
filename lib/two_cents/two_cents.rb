3fad3349006c39bb8995fdb4f1d7563ceede4faa
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-05 09:44:29 -0500

    fix

--- lib/two_cents/two_cents.rb
+++ lib/two_cents/two_cents.rb
@@ -7,18 +7,18 @@ module TwoCents
     end
 
 
-    def self.next
-      print "What would you like to do? "
-      input = STDIN.gets.strip.downcase.gsub(" ", "+")
-      if input == "fortune"
-        TwoCents.fortune
-      elsif input == "search"
-        TwoCents.search
-      else
-        put "Invalid - please fortune or search"
-      end
-    end
-    
+    # def self.next
+    #   print "What would you like to do? "
+    #   input = STDIN.gets.strip.downcase.gsub(" ", "+")
+    #   if input == "fortune"
+    #     TwoCents.fortune
+    #   elsif input == "search"
+    #     TwoCents.search
+    #   else
+    #     put "Invalid - please fortune or search"
+    #   end
+    # end
+
     def self.search(input=nil)
       print "What would you like to search? "
       input = STDIN.gets.strip.downcase.gsub(" ", "+")
@@ -30,43 +30,43 @@ module TwoCents
       if i == 1
         puts Cat.cat_1
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 2
         puts Cat.cat_2
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 3
         puts Cat.cat_3
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 4
         puts Cat.cat_4
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 5
         puts Cat.cat_5
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 6
         puts Cat.cat_6
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 7
         puts Cat.cat_7
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 8
         puts Cat.cat_8
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       elsif i == 9
         puts Cat.cat_9
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       else i == 10
         puts Cat.cat_10
         TwoCents.cents
-        TwoCents.next
+        TwoCents.search
       end
     end
 
